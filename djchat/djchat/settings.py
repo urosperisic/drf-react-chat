86653db9f926dec1c5a03226065e3f5b96820014
urosperisic <uros2907@gmail.com>
2025-07-10 21:11:38 +0200

    Initiate Chat Server Administration App. Creating a Django Custom User Model (Abstract User)

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "account",
+    "server",
 ]
 
 MIDDLEWARE = [
@@ -113,3 +115,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
