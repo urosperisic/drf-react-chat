dfd9985ee674ac38542a3c69927e2bffbde09b26
urosperisic <uros2907@gmail.com>
2025-07-10 23:27:09 +0200

    API Documentation: DRF-Spectacular with Swagger UI

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External
+    "drf_spectacular",
+    # Internal
     "account",
     "server",
 ]
@@ -117,3 +120,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+}
