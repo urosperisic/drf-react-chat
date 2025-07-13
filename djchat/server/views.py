9fa36fba3fe4effef3561a401b62605d6aff3f47
urosperisic <uros2907@gmail.com>
2025-07-13 21:47:55 +0200

    API Doc: Creating an API Endpoint Decorator for Detailing Endpoints

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
+from .schema import server_list_docs
 from .serializer import ServerSerializer
 
 
@@ -17,6 +18,7 @@ class ServerListViewSet(viewsets.ViewSet):
 
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
         """
         Retrieves a list of servers based on the provided query parameters.
@@ -90,29 +92,32 @@ class ServerListViewSet(viewsets.ViewSet):
         by_serverid = request.query_params.get("by_serverid")
         with_num_members = request.query_params.get("with_num_members") == "true"
 
-        if (by_user or by_serverid) and not request.user.is_authenticated:
-            raise AuthenticationFailed()
-
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
         if by_user:
-            user_id = request.user.id
-            self.queryset = self.queryset.filter(member=user_id)
+            if by_user and request.user.is_authenticated:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed()
 
         if with_num_members:
             self.queryset = self.queryset.annotate(num_members=Count("member"))
 
-        if qty:
-            self.queryset = self.queryset[: int(qty)]
-
         if by_serverid:
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
                     raise ValidationError(detail=f"Server with id {by_serverid} not found")
             except ValueError:
                 raise ValidationError(detail="Server value error")
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
 
         serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
