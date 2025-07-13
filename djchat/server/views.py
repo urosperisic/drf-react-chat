b4230e7c0babae3e35558c76b7132f74a2ffced1
urosperisic <uros2907@gmail.com>
2025-07-13 19:29:06 +0200

    Creating an  API Endpoint for Filtering Servers by Number of Members. Exclude from /select

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
@@ -15,6 +16,7 @@ class ServerListViewSet(viewsets.ViewSet):
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if (by_user or by_serverid) and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -25,6 +27,9 @@ class ServerListViewSet(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
         if qty:
             self.queryset = self.queryset[: int(qty)]
@@ -37,7 +42,7 @@ class ServerListViewSet(viewsets.ViewSet):
             except ValueError:
                 raise ValidationError(detail="Server value error")
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
 
 
